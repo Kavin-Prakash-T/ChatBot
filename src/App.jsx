90fee52adf964a83354ba50b0442ebab7934f554
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2026-01-05 14:37:38 +0530

    feat: add ChatMessage component to display chat messages in the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useEffect, useState } from 'react';
 import './App.css'
 import Header from './components/Header'
+import ChatMessage from './components/ChatMessage';
 
 function App() {
 
-  const [darkMode, setDarkMode] = useState(localStorage.getItem('darkMode')==='true' || false);
+  const [darkMode, setDarkMode] = useState(localStorage.getItem('darkMode') === 'true' || false);
+  const [messages, setMessages] = useState([
+    {
+      id: 1,
+      text: "Hello, how can I help you?",
+      sender: "bot",
+      timestamp: new Date()
+    },
+  ]);
 
   useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode))
@@ -16,9 +25,16 @@ function App() {
     }
   }, [darkMode]);
 
+
+
   return (
     <div className='flex flex-col h-screen'>
       <Header setDarkMode={setDarkMode} darkMode={darkMode} />
+      <div className='flex-1 overflow-y-auto p-4 md:p-6'>
+        <div className='max-w-5xl mx-auto space-y-4'>
+          <ChatMessage messages={messages} />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/ChatMessage.jsx
@@ -0,0 +1,23 @@
+import { Bot, User } from "lucide-react"
+
+const ChatMessage = ({messages}) => {
+  return (
+    <div className={`${messages.sender==='user'?"justify-end":"justify-start"}`}>
+        <div className={`flex max-w-[80%] md:max-w-[70%] rounded-2xl p-5 py-3.5 ${messages.sender==='user'?"bg-linear-to-r from-indigo-600 to-purple-600  text-white shadow-md":"bg-white text-gray-800 shadow-md dark:bg-gray-800 dark:text-gray-200 dark:border-gray-700"}`}>
+            <div className={`shrink-0 mr-3 ${messages.sender==='user'?"text-indigo-200":"text-indigo-600 dark:text-indigo-400"}`}>
+                {messages.sender==='user' ? <User className="h-5 w-5" /> : <Bot className="h-5 w-5" />}
+            </div>
+            <div className="flex-1">
+                <div className="mb-1 flex justify-between items-center">
+                    <span className="font-medium">
+                        {messages.sender==='user' ? "You" : "AI Assistant"}
+                    </span>
+                    <span className={`text-xs ${messages.sender==='user'? "opacity-70": "text-gray-500 dark:text-gray-400"}`}></span>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ChatMessage
